update suffix gradient & fix prefix

self_control/suffix_gradient/generate_delta_ds.py
@@ -15,6 +15,7 @@ import json
 import pickle
 from tqdm import tqdm
 import matplotlib.pyplot as plt
+from torch.utils.data import Dataset, DataLoader
 from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 from self_control.suffix_gradient.repe import WrappedReadingVecModel
 import torch.nn.functional as F
@@ -22,20 +23,75 @@ from peft import AdaptionPromptConfig, get_peft_model, LoraModel, LoraConfig, pr
 from self_control.utils import SuffixItem
 import argparse
 
+import warnings
+
+from copy import deepcopy
+
+def clean_padded_gradients(grad_dict, original_lengths):
+    """
+    Remove padding from the gradients in the dictionary.
+
+    Args:
+    - grad_dict (dict): A dictionary where keys are parameter names and values are gradient tensors.
+    - original_lengths (torch.Tensor or list): The original lengths of the sequences before padding.
+
+    Returns:
+    - list: A new list with the padding removed from the gradients.
+    """
+    cleaned_grad_list = []
+    batch_size = len(original_lengths)
+    for i in range(batch_size):
+        temp_grad = {}
+        for layer, grad in grad_dict.items():
+            temp_grad[layer] = grad[i, -original_lengths[i]:]
+        # Assume the sequence length dimension is the second dimension (dim=1)
+        cleaned_grad_list.append(deepcopy(temp_grad))
+    return cleaned_grad_list
+
+def clean_padded_hiddens(hiddens, original_lengths):
+    """
+    Remove padding from the hiddens
+
+    Args:
+    - hiddens (tuple): A tuple of hidden states
+    - original_lengths (torch.Tensor or list): The original lengths of the sequences before padding.
+
+    Returns:
+    - list: A new list with the padding removed from the hidden states.
+    """
+    cleaned_hidden_list = []
+    batch_size = hiddens[0].size(0)
+    for i in range(batch_size):
+        temp_hidden = []
+        for layer in range(len(hiddens)):
+            temp_hidden.append(hiddens[layer][i, -original_lengths[i]:])
+        temp_hidden = torch.stack([hidden for hidden in temp_hidden])
+        cleaned_hidden_list.append(temp_hidden)
+    return cleaned_hidden_list
+
 parser = argparse.ArgumentParser()
 parser.add_argument("--attribute", type=str, default="happy", help="The attribute of the seed queries to generate")
 parser.add_argument("--data_path", type=str, default=None, help="Path of a dataset")
 parser.add_argument("--output_name", type=str, default=None, help="Name of the output file")
-parser.add_argument("--max_num_data", type=int, default=1000, help="Max number of data item")
+parser.add_argument("--max_num_data", type=int, default=None, help="Max number of data item")
+parser.add_argument("--start_from_idx", type=int, default=0, help="Start index of the data")
+parser.add_argument("--batchsize", type=int, default=6, help="Batch size")
+parser.add_argument("--epoch", type=int, default=1, help="Epoch of data generation, should be used with sampling")
+
+# control_generate
+parser.add_argument("--search", action="store_true")
+parser.add_argument("--init_coeff", type=float, default=-0.5, help="Coefficient for control. Will serve as the initial coeff if search=True")
+parser.add_argument("--iteration", type=int, default=1, help="Number of iterations of control")
+parser.add_argument("--random_seed", type=int, default=42, help="Random seed")
+parser.add_argument("--do_sample", action="store_true")
+parser.add_argument("--return_hiddens", action="store_true")
 args = parser.parse_args()
 
 
-# model_name_or_path = "/home/models/llama2-7b-chat-hf/"
 model_name_or_path = "meta-llama/Llama-2-7b-chat-hf"
-model = AutoModelForCausalLM.from_pretrained(model_name_or_path, torch_dtype=torch.float16, device_map="auto").eval()
-# model = AutoModelForCausalLM.from_pretrained(model_name_or_path, torch_d|type=torch.float32, device_map="auto", token=True).eval()
-use_fast_tokenizer = "LlamaForCausalLM" not in model.config.architectures
-tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
+model = AutoModelForCausalLM.from_pretrained(model_name_or_path, torch_dtype=torch.bfloat16, device_map="auto").eval()
+# model = AutoModelForCausalLM.from_pretrained(model_name_or_path, torch_dtype=torch.float32, device_map="auto", token=True).eval()
+tokenizer = AutoTokenizer.from_pretrained(model_name_or_path, use_fast=True, padding_side="left")
 tokenizer.pad_token_id = 0 if tokenizer.pad_token_id is None else tokenizer.pad_token_id
 tokenizer.bos_token_id = 1
 
@@ -67,18 +123,27 @@ else:
 print("Using data from ", seed_data_dir)
 prompts = []
 data_counter = 0
+if args.max_num_data is None:
+    args.max_num_data = float('inf')
+print(args.max_num_data)
 with open(seed_data_dir, "r") as f:
-    for line in f:
-        if data_counter >= args.max_num_data:
-            break
-        data_counter += 1
-        data = json.loads(line)
-        if "query" in data:
-            prompts.append(data["query"])
-        elif "question" in data:
-            prompts.append(data["question"]) # gsm8k
-        else:
-            ValueError(f"Unkown structure of data from path {seed_data_dir}")
+    if "jsonl" in seed_data_dir:
+        for line in f:
+            if data_counter < args.start_from_idx:
+                data_counter += 1
+                continue
+            if data_counter >= args.start_from_idx + args.max_num_data:
+                break
+            data_counter += 1
+            data = json.loads(line)
+            if "query" in data:
+                prompts.append(data["query"])
+            elif "question" in data:
+                prompts.append(data["question"]) # gsm8k
+            else:
+                ValueError(f"Unkown structure of data from path {seed_data_dir}")
+    else:
+        prompts = eval(f.read())[args.start_from_idx:args.start_from_idx + args.max_num_data]
 
 data_id = len([name for name in os.listdir(target_dir) if os.path.isfile(os.path.join(target_dir, name))])
 if args.output_name is not None:
@@ -88,36 +153,85 @@ else:
 print("Target path: ", delta_data_dir)
 input_and_grads = []
 
-for input_text in prompts:
-    input_text = f"[INST] {input_text} [/INST]"
-    controlled_output, grad_list = wrapped_model.controlled_generate(
-        prompt=input_text,
-        suffix=suffix,
-        loss_fct=loss_fct,
-        coeff=-0.4,
-        iterations=1,
-        random_seed=42,
-        smoothing=0,
-        max_new_tokens=200,
-        return_intermediate=True,
-        gradient_manipulation="clipping",
-        save_intermediate_states=True,
-        norm=1,
-        annealing=1,
-        use_cache=False,
-    )
-    # Store the pair of input text and its corresponding hidden states
-    for i, grad in enumerate(grad_list):
-        with open(delta_data_dir, 'ab') as f:
-            pickle.dump((input_text, grad), f)
-# Now you have a list where each element is a tuple (input_text, hidden_states)
+class DeltaDataset(Dataset):
+    def __init__(self, data, tokenizer):
+        self.data = data
+        self.tokenizer = tokenizer
 
-# Save the data
-# You can choose the format that best suits your needs, for example, pickle
+    def __len__(self):
+        return len(self.data)
 
+    def __getitem__(self, idx):
+        data_item = f"[INST] {self.data[idx]} [/INST]"
+        query_len = self.tokenizer(data_item, return_tensors="pt")["input_ids"].size(1)
+        return {
+            "input_str": data_item,
+            "query_len": query_len
+        }
+    
+def delta_collate_fn(batch):
+    input_str_list = [item['input_str'] for item in batch]
+    query_len_list = [item['query_len'] for item in batch]
+    inputs = tokenizer(input_str_list, return_tensors="pt", padding=True)
+    inputs["input_ids"] = inputs["input_ids"].to(model.device)
+    inputs["attention_mask"] = inputs["attention_mask"].to(model.device)
 
+    return {
+        "input_str": input_str_list,
+        "query_len": query_len_list,
+        "input_ids": inputs["input_ids"],
+        "attention_mask": inputs["attention_mask"]
+    }
+print(prompts)
+delta_dataset = DeltaDataset(prompts, tokenizer)
+print("Example data:\n", delta_dataset[0])
+dataloader = DataLoader(delta_dataset, batch_size=args.batchsize, collate_fn=delta_collate_fn)
 
+pbar = tqdm(total=args.epoch*len(dataloader))
+do_sample = False
+if args.epoch > 1 or args.do_sample:
+    print("Sampling data")
+    do_sample = True
+for _ in range(args.epoch):
+    for batch in dataloader:
+        input_ids = batch["input_ids"]
+        attention_mask = batch["attention_mask"]
+        query_len = batch["query_len"]
+        input_str = batch["input_str"]
+        try:
+            grad_list = wrapped_model.controlled_generate(
+                input_ids=input_ids,
+                attention_mask=attention_mask,
+                suffix=suffix,
+                loss_fct=loss_fct,
+                coeff=args.init_coeff,
+                iterations=args.iteration,
+                random_seed=args.random_seed,
+                smoothing=0,
+                max_new_tokens=200,
+                return_intermediate=True,
+                return_hiddens=args.return_hiddens,
+                gradient_manipulation="clipping",
+                save_intermediate_states=True if not args.return_hiddens else False,
+                norm=1,
+                annealing=1,
+                use_cache=False,
+                do_sample=do_sample,
+            )
+            # print(grad_list)
+            if not args.return_hiddens:
+                cleaned_grad_list = clean_padded_gradients(grad_list, query_len)
+            else:
+                cleaned_grad_list = clean_padded_hiddens(grad_list, query_len)
+            # Store the pair of input text and its corresponding hidden states
+            for i, (input_text, grad) in enumerate(zip(input_str, cleaned_grad_list)):
+                with open(delta_data_dir, 'ab') as f:
+                    pickle.dump((input_text, grad), f)
+        except Exception as e:
+            print(e)
+        pbar.update(1)
 
-# To load the hidden states and inputs later
-with open(delta_data_dir, 'rb') as f:
-    loaded_data = pickle.load(f)
+"""
+usage: CUDA_VISIBLE_DEVICES=3 python -m self_control.suffix_gradient.generate_delta_ds --attribute happy --data_path benchmarks/emotions/happiness.json \
+    --output_name happy_search --epoch 5 --search --init_coeff -2 --iteration 2
+"""

self_control/suffix_gradient/repe/rep_control_reading_vec.py
@@ -10,6 +10,7 @@ from peft import PeftModel
 
 import transformers
 import random
+from copy import deepcopy
 
 class WrappedBlock(torch.nn.Module):
     def __init__(self, block):
@@ -19,7 +20,6 @@ class WrappedBlock(torch.nn.Module):
         self.controller = None
         self.mask = None
         self.token_pos = None
-        self.normalize = False
 
     def forward(self, *args, **kwargs):
         output = self.block(*args, **kwargs)
@@ -30,20 +30,8 @@ class WrappedBlock(torch.nn.Module):
         else:
             self.output = output
             modified = output
-
-
-        # print(modified.shape)
-        # print(kwargs.keys())
-
-            
-            
-        # print(modified.shape)
-        # print(kwargs.keys())
-
             
         if self.controller is not None:
-            norm_pre = torch.norm(modified, dim=-1, keepdim=True)
-
             if self.mask is not None:
                 mask = self.mask
 
@@ -91,10 +79,6 @@ class WrappedBlock(torch.nn.Module):
                     assert False, f"Unknown token position {self.token_pos}."
             else:
                 modified = modified + self.controller * mask
-
-            if self.normalize:
-                norm_post = torch.norm(modified, dim=-1, keepdim=True)
-                modified = modified / norm_post * norm_pre
         
         # modified = torch.clamp(modified, output[0]-0.1, output[0]+0.1)
         if isinstance(output, tuple):
@@ -104,8 +88,7 @@ class WrappedBlock(torch.nn.Module):
         
         return output
 
-    def set_controller(self, activations, token_pos=None, masks=None, normalize=False):
-        self.normalize = normalize
+    def set_controller(self, activations, token_pos=None, masks=None):
         self.controller = activations
         self.mask = masks
         self.token_pos = token_pos
@@ -163,11 +146,11 @@ class WrappedReadingVecModel(torch.nn.Module):
                 print("using list")
                 activations[layer_id] = grads[layer_id][:, :, :]
 
-            self.set_controller(layer_id, activations[layer_id], token_pos=token_pos, masks=1, normalize=False)
+            self.set_controller(layer_id, activations[layer_id], token_pos=token_pos, masks=1)
 
 
     def controlled_generate(self,
-                            prompt: str=None,
+                            prompt: List[str]=None,
                             input_ids=None,
                             attention_mask=None,
                             suffix: Union[SuffixItem, List[SuffixItem]]=None,
@@ -175,13 +158,14 @@ class WrappedReadingVecModel(torch.nn.Module):
                             verbalizer=None,
                             coeff=-0.1,
                             iterations=5,
+                            k=1,
+                            max_search_steps=3,
                             token_pos="start",
                             layer_ids=list(range(0, 32, 1)),
                             random_seed=0,
                             consistent=True,
                             use_last=False,
                             use_cache=False,
-                            keep_input=False,
                             smoothing: float = 0,
                             search=False,
                             verbose=False,
@@ -189,8 +173,11 @@ class WrappedReadingVecModel(torch.nn.Module):
                             return_intermediate=False,
                             return_hiddens=False,
                             return_grads=False,
+                            return_logits=False,
+                            return_ids=False,
                             remain_control=False,
                             load_best_last=False,
+                            last_max_new_tokens=None,
                             save_intermediate_states=False,
                             do_sample=False,
                             norm=1,
@@ -208,16 +195,17 @@ class WrappedReadingVecModel(torch.nn.Module):
         self.model.eval()
         self.reset()
         best_loss = float('inf')
-        all_grads = []
+        final_grads = {}
         temp_grads = {}
         acc_grads = {}  # accumulated gradients
         best_grads = {}
         gradient_bs = 1 # TODO: default to 1
+        orig_coeff = coeff
 
         # Prepare inputs
         inputs = {}
         if prompt is not None:
-            inputs = self.tokenizer(prompt, return_tensors="pt")
+            inputs = self.tokenizer(prompt, return_tensors="pt", padding=True)
             inputs["input_ids"] = inputs["input_ids"].to(self.model.device)
             inputs["attention_mask"] = inputs["attention_mask"].to(self.model.device)
             query_length = inputs["input_ids"].size(1)
@@ -228,16 +216,18 @@ class WrappedReadingVecModel(torch.nn.Module):
         if return_intermediate:
             iterative_outputs = []
         controlled_output = self.generate(**inputs, use_cache=use_cache, do_sample=do_sample, **kwargs) # the original output
-        original_output = controlled_output
+        original_output = deepcopy(controlled_output)
         if verbose:
-            print("Original Output:\n", controlled_output)
-            rationale = self.generate(controlled_output+suffix.suffix, use_cache=use_cache, do_sample=do_sample, **kwargs)
-            print("Rationale:\n", rationale)
-            print("="*50)
+            print("Coeff: ", orig_coeff)
+        #     print("Original Output:\n", controlled_output)
+        #     rationale = self.generate([output+suffix.suffix for output in controlled_output], use_cache=use_cache, do_sample=do_sample, **kwargs)
+        #     print("Rationale:\n", rationale)
+        #     print("="*50)
 
-        for iter in tqdm(range(iterations)):
+        for iter in range(iterations):
             # print(controlled_output)
-            controlled_output = controlled_output + suffix.suffix
+            for i in range(len(controlled_output)):
+                controlled_output[i] = controlled_output[i] + suffix.suffix
 
             if isinstance(suffix, list):
                 warnings.warn(f"Accepting a list of suffixes has not been tested right now")
@@ -257,6 +247,7 @@ class WrappedReadingVecModel(torch.nn.Module):
                         verbalizer=verbalizer,
                         smoothing=smoothing,
                         norm=norm,
+                        gradient_manipulation=gradient_manipulation,
                     )
                     composed_grads = {k: (composed_grads[k] + grads[k] * suffix_item.direction) if k in composed_grads else grads[k]\
                                        for k in set(grads)}
@@ -284,35 +275,34 @@ class WrappedReadingVecModel(torch.nn.Module):
             if loss < best_loss:
                 best_loss = loss
                 best_grads = acc_grads
-
             if search:
                 step_size = bidirectional_line_search(
                     orig_input          =   prompt,
                     suffix              =   suffix.suffix,
                     wrapped_model       =   self,
                     acc_grads           =   acc_grads,
-                    initial_step_size   =   0.2,
+                    initial_step_size   =   orig_coeff,
+                    verbose             =   verbose,
+                    max_iterations      =   max_search_steps,
+                    initial_grads_loss  =   {
+                        "grads": grads,
+                        "loss": loss,
+                        "controlled_output": controlled_output
+                    },
                     # control args
-                    model               =   self.model,
                     tokenizer           =   self.tokenizer,
                     target              =   target_token,
                     query_length        =   query_length,
                     verbalizer          =   verbalizer,
                     loss_fct            =   loss_fct,
+                    **kwargs
                 )
                 coeff = step_size
-
             for i in grads:
                 if i in acc_grads:
                     acc_grads[i] = acc_grads[i][:, :query_length] + coeff * grads[i][:, :query_length]
                 else:
                     acc_grads[i] = coeff * grads[i][:, :query_length]
-                if save_intermediate_states:
-                    temp_grads[i] = acc_grads[i].cpu().detach().clone()
-            if save_intermediate_states:
-                print("Saving")
-                all_grads.append(temp_grads)
-                temp_grads = {}
             coeff *= annealing
                     
             self.control_on_layers(
@@ -324,18 +314,18 @@ class WrappedReadingVecModel(torch.nn.Module):
                 epsilon=epsilon
             )
 
-            controlled_output = self.generate(**inputs, keep_input=True, use_cache=use_cache, do_sample=do_sample, **kwargs)
+            controlled_output = self.generate(**inputs, use_cache=use_cache, do_sample=do_sample, **kwargs)
             if not consistent:
                 self.reset()
-            if verbose:
-                print(f"Loss from the iteration {iter}: {loss.item()}")
-                print(f"Output form the iteration {iter}:\n", controlled_output)
-                rationale = self.generate(controlled_output+suffix.suffix, keep_input=False, use_cache=use_cache, do_sample=do_sample, **kwargs)
-                print("Rationale:\n", rationale)
-                print("="*50)
+            # if verbose:
+            #     print(f"Loss from the iteration {iter}: {loss.item()}")
+            #     print(f"Output form the iteration {iter}:\n", controlled_output)
+            #     rationale = self.generate([output + suffix.suffix for output in controlled_output], keep_input=False, use_cache=use_cache, do_sample=do_sample, **kwargs)
+            #     print("Rationale:\n", rationale)
+            #     print("="*50)
             if return_intermediate:
-                iterative_outputs.append(controlled_output)
-        controlled_output = controlled_output + suffix.suffix
+                iterative_outputs.append(deepcopy(controlled_output))
+        controlled_output = [output + suffix.suffix for output in controlled_output]
         grads, outputs, loss, probs, logits, norms = get_verbalized_grads_from_wrapped_model(
             wrapped_model=self,
             tokenizer=self.tokenizer,
@@ -345,10 +335,14 @@ class WrappedReadingVecModel(torch.nn.Module):
             verbalizer=verbalizer,
             smoothing=smoothing,
             norm=norm,
+            gradient_manipulation=gradient_manipulation,
         )
         if loss < best_loss:
             best_loss = loss
             best_grads = acc_grads
+        if save_intermediate_states:
+            for i in best_grads:
+                final_grads[i] = best_grads[i].cpu().detach().clone()
         if load_best_last:
             self.control_on_layers(
                 layer_ids=layer_ids,
@@ -356,20 +350,22 @@ class WrappedReadingVecModel(torch.nn.Module):
                 query_length=query_length,
                 token_pos=token_pos,
             )
-            controlled_output = self.generate(**inputs, keep_input=True, use_cache=use_cache, do_sample=do_sample, **kwargs)
+        if last_max_new_tokens is not None:
+            kwargs.pop("max_new_tokens")
+            controlled_output = self.generate(**inputs, use_cache=use_cache, do_sample=do_sample, return_ids=return_ids, max_new_tokens=last_max_new_tokens, **kwargs) # only pass return_ids here
+        else:
+            controlled_output = self.generate(**inputs, use_cache=use_cache, do_sample=do_sample, return_ids=return_ids, **kwargs) # only pass return_ids here
         # TODO: return a tuple
-        if not remain_control:
+        if return_logits:
+            outputs = self.model(**inputs, output_hidden_states=True)
+            return outputs.logits
+        if not remain_control and not return_hiddens:
             self.reset()
         if save_intermediate_states:
-            return controlled_output, all_grads
+            return controlled_output, final_grads
         if return_grads:
             return controlled_output, best_grads
         if return_hiddens:
-            tokenized = self.tokenizer(prompt, return_tensors="pt")
-            input_ids = tokenized.input_ids
-            attention_mask = torch.ones_like(input_ids)
-            inputs["input_ids"] = input_ids
-            inputs["attention_mask"] = attention_mask
             outputs = self.model(**inputs, output_hidden_states=True)
             return outputs['hidden_states'][1:]
         if return_intermediate:
@@ -379,11 +375,11 @@ class WrappedReadingVecModel(torch.nn.Module):
 
         
     def generate(self,
-                 prompt: str=None,
-                 keep_input=False,
+                 prompt: List[str]=None,
+                 return_ids=False,
                  **kwargs):
         if prompt is not None:
-            inputs = self.tokenizer(prompt, return_tensors="pt")
+            inputs = self.tokenizer(prompt, return_tensors="pt", padding=True)
             inputs["input_ids"] = inputs["input_ids"].to(self.model.device)
             inputs["attention_mask"] = inputs["attention_mask"].to(self.model.device)
             gen_ids = self.model.generate(**inputs, **kwargs)
@@ -396,11 +392,14 @@ class WrappedReadingVecModel(torch.nn.Module):
         #     )
         #     return ground_truth_generation
         # else:
-        ground_truth_generation = self.tokenizer.decode(
-            gen_ids[0],
-            skip_special_tokens=True,
-        )
-        return ground_truth_generation
+        if return_ids:
+            return gen_ids
+        else:
+            ground_truth_generation = self.tokenizer.batch_decode(
+                gen_ids,
+                skip_special_tokens=True,
+            )
+            return ground_truth_generation
         
     def get_past_kvs(self, prompt, **kwargs):
         inputs_embeds = get_sentence_embedding(
@@ -643,50 +642,50 @@ class WrappedReadingVecModel(torch.nn.Module):
             return _get_activations(layer_ids, block_name)
 
 
-    def set_controller(self, layer_ids, activations, block_name='decoder_block', token_pos=None, masks=None, normalize=False):
+    def set_controller(self, layer_ids, activations, block_name='decoder_block', token_pos=None, masks=None):
 
-        def _set_controller(layer_id, activations, block_name, masks, normalize):
+        def _set_controller(layer_id, activations, block_name, masks):
             current_layer = self.model.model.layers[layer_id]
 
             if block_name == 'decoder_block':
-                current_layer.set_controller(activations, token_pos, masks, normalize)
+                current_layer.set_controller(activations, token_pos, masks)
             elif self.is_wrapped(current_layer):
                 current_block = current_layer.block
                 if block_name == 'kv' and self.is_wrapped(current_block.k_proj) and self.is_wrapped(current_block.v_proj):  # to be able to control kv separately
-                    current_block.k_proj.set_controller(activations, token_pos, masks, normalize)
-                    current_block.v_proj.set_controller(activations, token_pos, masks, normalize)
+                    current_block.k_proj.set_controller(activations, token_pos, masks)
+                    current_block.v_proj.set_controller(activations, token_pos, masks)
                 elif block_name == 'self_attn' and self.is_wrapped(current_block.self_attn):
-                    current_block.self_attn.set_controller(activations, token_pos, masks, normalize)
+                    current_block.self_attn.set_controller(activations, token_pos, masks)
                 elif block_name == 'mlp' and self.is_wrapped(current_block.mlp):
-                    current_block.mlp.set_controller(activations, token_pos, masks, normalize)
+                    current_block.mlp.set_controller(activations, token_pos, masks)
                 elif block_name == 'input_layernorm' and self.is_wrapped(current_block.input_layernorm):
-                    current_block.input_layernorm.set_controller(activations, token_pos, masks, normalize)
+                    current_block.input_layernorm.set_controller(activations, token_pos, masks)
                 elif block_name == 'post_attention_layernorm' and self.is_wrapped(current_block.post_attention_layernorm):
-                    current_block.post_attention_layernorm.set_controller(activations, token_pos, masks, normalize)
+                    current_block.post_attention_layernorm.set_controller(activations, token_pos, masks)
                 else:
                     return f"No wrapped block named {block_name}."
 
             else:
                 if block_name == 'kv' and self.is_wrapped(current_layer.self_attn.k_proj) and self.is_wrapped(current_layer.self_attn.v_proj):  # to be able to control kv separately
-                    current_layer.k_proj.set_controller(activations, token_pos, masks, normalize)
-                    current_layer.v_proj.set_controller(activations, token_pos, masks, normalize)
+                    current_layer.k_proj.set_controller(activations, token_pos, masks)
+                    current_layer.v_proj.set_controller(activations, token_pos, masks)
                 if block_name == 'self_attn' and self.is_wrapped(current_layer.self_attn):
-                    current_layer.self_attn.set_controller(activations, token_pos, masks, normalize)
+                    current_layer.self_attn.set_controller(activations, token_pos, masks)
                 elif block_name == 'mlp' and self.is_wrapped(current_layer.mlp):
-                    current_layer.mlp.set_controller(activations, token_pos, masks, normalize)
+                    current_layer.mlp.set_controller(activations, token_pos, masks)
                 elif block_name == 'input_layernorm' and self.is_wrapped(current_layer.input_layernorm):
-                    current_layer.input_layernorm.set_controller(activations, token_pos, masks, normalize)
+                    current_layer.input_layernorm.set_controller(activations, token_pos, masks)
                 elif block_name == 'post_attention_layernorm' and self.is_wrapped(current_layer.post_attention_layernorm):
-                    current_layer.post_attention_layernorm.set_controller(activations, token_pos, masks, normalize)
+                    current_layer.post_attention_layernorm.set_controller(activations, token_pos, masks)
                 else:
                     return f"No wrapped block named {block_name}."
                 
         if isinstance(layer_ids, list) or isinstance(layer_ids, tuple) or isinstance(layer_ids, np.ndarray):
             assert isinstance(activations, dict), "activations should be a dictionary"
             for layer_id in layer_ids:
-                _set_controller(layer_id, activations[layer_id], block_name, masks, normalize)
+                _set_controller(layer_id, activations[layer_id], block_name, masks)
         else:
-            _set_controller(layer_ids, activations, block_name, masks, normalize)
+            _set_controller(layer_ids, activations, block_name, masks)
       
         
     def reset(self):

self_control/utils/utils.py
@@ -5,6 +5,12 @@ from typing import List, Dict, Tuple
 from copy import deepcopy
 from scipy.special import softmax
 from transformers import AutoModelForCausalLM, AutoTokenizer
+from torch.nn.functional import kl_div
+import numpy as np
+
+from torch import nn
+from torch.nn import functional as F
+from torch.func import functional_call, vmap
 # from self_control.suffix_gradient.repe import WrappedReadingVecModel
 
 def loss_over_multiple_next_tokens(model, inputs, loss_fct, targets):
@@ -125,8 +131,9 @@ def get_verbalized_grads_from_wrapped_model(wrapped_model,
                                             loss_fct,
                                             targets,
                                             verbalizer: List[int],
-                                            smoothing=0.5,
+                                            smoothing=0,
                                             norm=1,
+                                            k=1,
                                             gradient_manipulation: str="clipping"
                                             ):
     """
@@ -140,7 +147,16 @@ def get_verbalized_grads_from_wrapped_model(wrapped_model,
     Returns:
     - torch.Tensor: The cross entropy loss.
     """
-    tokenized = tokenizer(inputs, return_tensors="pt")
+    def normalize(x):
+        t = (x ** 2).view(x.shape[0], -1).sum(-1).sqrt()
+
+        return x / (t.view(-1, *([1] * x.shape[1:])) + 1e-12)
+    def L2_norm(x, keepdim=False):
+        z = (x ** 2).view(x.shape[0], -1).sum(-1).sqrt()
+        if keepdim:
+            z = z.view(-1, *[1]*(len(x.shape) - 1))
+        return z
+    tokenized = tokenizer(inputs, return_tensors="pt", padding=True)
     tokenized["input_ids"] = tokenized["input_ids"].to(wrapped_model.model.device)
     tokenized["attention_mask"] = tokenized["attention_mask"].to(wrapped_model.model.device)
     with torch.enable_grad():
@@ -150,8 +166,8 @@ def get_verbalized_grads_from_wrapped_model(wrapped_model,
             attention_mask=tokenized["attention_mask"],
             output_hidden_states=True,
         )
-        one_hot_dist = torch.zeros(1, outputs.logits.shape[-1])
-        one_hot_dist[0, targets[0].cpu().numpy()] = 1
+        one_hot_dist = torch.zeros(outputs.logits.size(0), outputs.logits.shape[-1])
+        one_hot_dist[:, targets[0].cpu().numpy()] = 1
         one_hot_dist = label_smoothing(one_hot_dist, smoothing=smoothing)
         loss = loss_fct(outputs.logits[:, -1, :], one_hot_dist.to(wrapped_model.model.device))
 
@@ -159,7 +175,6 @@ def get_verbalized_grads_from_wrapped_model(wrapped_model,
         norms = {}
         hidden_states = outputs.hidden_states[1:] # outputs.hidden_states[0] is the embedding layer
         if gradient_manipulation == "pgd":
-            # print(hidden_states)
             X_pgd = {}
             for i in range(len(hidden_states)):
                 X_pgd[i] = hidden_states[i].clone()
@@ -173,15 +188,24 @@ def get_verbalized_grads_from_wrapped_model(wrapped_model,
                 norms[i][norm_mask] = 1
                 grads[i] = grads[i] / norms[i]
             elif gradient_manipulation == "pgd":
-                step_size = -1e-3
+                step_size = 0.5
                 epsilon = 0.3
-                eta = step_size * grads[i].data
+                # max_val = torch.max(torch.abs(grads[i]))
+                # grads[i] = grads[i] / max_val
+                # pgd_norm = torch.norm(grads[i], dim=-1, p=2, keepdim=True)
+                # grads[i] = max_val * grads[i] / pgd_norm
+                # norm_mask = norms[i] <= norm
+                # norms[i][norm_mask] = 1
+                eta = step_size * grads[i] / (norms[i] + 1e-12)
                 X_pgd[i] = X_pgd[i].data + eta
-                eta = torch.clamp(X_pgd[i].data - hidden_states[i].data, -epsilon, epsilon)
-                X_pgd[i] = Variable(hidden_states[i].data + eta, requires_grad=True)
+                X_pgd[i] = torch.clamp(X_pgd[i], hidden_states[i] - epsilon, hidden_states[i] + epsilon)
+                # eta = torch.clamp(X_pgd[i].data - hidden_states[i].data, -epsilon, epsilon)
+                # X_pgd[i] = Variable(hidden_states[i].data + eta, requires_grad=True)
                 # X_pgd[i] = Variable(torch.clamp(X_pgd[i], 0, 1.0), requires_grad=True)
                 grads[i] = X_pgd[i] - hidden_states[i]
-
+            elif gradient_manipulation == "autopgd":
+                pass
+        
         # print(grads)
         probs = softmax(outputs.logits[:, -1, verbalizer].detach().cpu().numpy()[0])
         logits = outputs.logits
@@ -200,13 +224,12 @@ def get_verbalized_grads(model, tokenizer, inputs: str, loss_fct, targets, verba
     Returns:
     - torch.Tensor: The cross entropy loss.
     """
-    ground_truth_embeds = get_sentence_embedding(
-        model, tokenizer, inputs
-    )
+    tokenized = tokenizer(inputs, return_tensors="pt", padding=True)
+    tokenized["input_ids"] = tokenized["input_ids"].to(model.model.device)
+    tokenized["attention_mask"] = tokenized["attention_mask"].to(model.model.device)
     outputs = model(
-        inputs_embeds=ground_truth_embeds,
-        # input_ids=inputs["input_ids"],
-        # attention_mask=inputs["attention_mask"],
+        input_ids=tokenized["input_ids"],
+        attention_mask=tokenized["attention_mask"],
         output_hidden_states=True,
     )
     loss = loss_fct(outputs.logits[:, -1, :], targets)
@@ -253,7 +276,7 @@ def control_on_layers(layer_ids, wrapped_model, grads, query_length, token_pos="
             print("using list")
             activations[layer_id] = grads[layer_id][:, :, :]
 
-        wrapped_model.set_controller(layer_id, activations[layer_id], token_pos=token_pos, masks=1, normalize=False)
+        wrapped_model.set_controller(layer_id, activations[layer_id], token_pos=token_pos, masks=1)
 
     return wrapped_model
 
@@ -391,6 +414,9 @@ def bidirectional_line_search(orig_input: str,
                                 loss_threshold: float=1e-5,
                                 max_iterations: int=3,
                                 scale_factor: float=0.5,
+                                initial_grads_loss: Dict=None,
+                                do_sample=False,
+                                verbose=False,
                                 **control_args
                                 ) -> float:
     """
@@ -409,83 +435,90 @@ def bidirectional_line_search(orig_input: str,
         The best step size
     """
     query_length = control_args.pop("query_length")
-    model = control_args.pop("model")
     tokenizer = control_args.pop("tokenizer")
     loss_fct = control_args.pop("loss_fct")
     target = control_args.pop("target")
     verbalizer = control_args.pop("verbalizer")
-
 
     # Initialize variables
     best_loss = float('inf')
     best_step_size = initial_step_size
     current_step_size = initial_step_size
     
-    input_with_suffix = wrapped_model.generate(orig_input, keep_input=True, random_seed=42) + suffix
-    grads, outputs, loss, probs, logits, norms = get_verbalized_grads(
-        inputs=input_with_suffix,
-        model=model,
-        tokenizer=tokenizer,
-        loss_fct=loss_fct,
-        targets=target,
-        verbalizer=verbalizer
-    )
-    for i in range(max_iterations):
-        # Check both directions
-        for direction in [-1, 1]:
-            # Modify the step-size for the current direction
-            test_step_size = direction * current_step_size
+    # grads, outputs, loss, probs, logits, norms = get_verbalized_grads_from_wrapped_model(
+    #     inputs=input_with_suffix,
+    #     wrapped_model=model,
+    #     tokenizer=tokenizer,
+    #     loss_fct=loss_fct,
+    #     targets=target,
+    #     verbalizer=verbalizer
+    # )
 
-            temp_grads = {}
-            for i in grads:
-                if i in acc_grads:
-                    # do max-length padding
-                    max_len = max(grads[i].size(1), acc_grads[i].size(1))
-                    acc_padding_needed = max_len - acc_grads[i].size(1)
-                    grad_padding_needed = max_len - grads[i].size(1)
-                    acc_grads[i] = torch.cat([acc_grads[i], torch.zeros(acc_grads[i].size(0), acc_padding_needed, acc_grads[i].size(2)).to(model.device)], dim=1)
-                    grads[i] = torch.cat([grads[i], torch.zeros(grads[i].size(0), grad_padding_needed, grads[i].size(2)).to(model.device)], dim=1)
-                    temp_grads[i] = acc_grads[i] + test_step_size * grads[i]    # should always use the initial grads
-                else:
-                    temp_grads[i] = test_step_size * grads[i]
-            
-            token_pos = "start"     # control on input tokens by default
-            layer_ids = list(range(0, 32, 1))   # control on all layers by default
-            wrapped_model = control_on_layers(
-                layer_ids=layer_ids,
-                wrapped_model=wrapped_model,
-                grads=temp_grads,
-                query_length=query_length,
-                token_pos=token_pos,
-            )
-            input_with_suffix = wrapped_model.generate(orig_input, keep_input=True, random_seed=42) + suffix
-            _, outputs, loss, probs, logits, norms = get_verbalized_grads(
-                inputs=input_with_suffix,
-                model=model,
-                tokenizer=tokenizer,
-                loss_fct=loss_fct,
-                targets=target,
-                verbalizer=verbalizer
-            )
-            
-            # Check if the loss is better than what we have seen so far
-            if loss < best_loss:
-                best_loss = loss
-                best_step_size = test_step_size
-                
-                # Check if the loss is below the threshold
-                if loss <= loss_threshold:
-                    print(f"Step-size found: {best_step_size}, Loss: {loss}")
-                    return best_step_size
+    # del outputs, probs, logits, norms
+    input_with_suffix = initial_grads_loss["controlled_output"]
+    loss = initial_grads_loss["loss"]
+    grads = initial_grads_loss["grads"]
+
+    if verbose:
+        print(f"Input w/ suffix: {input_with_suffix}")
+        print(f"Initial Loss: {loss}")
+
+    for i in range(max_iterations):
+        test_step_size = current_step_size
+
+        temp_grads = {}
+        for i in grads:
+            if i in acc_grads:
+                temp_grads[i] = acc_grads[i][:, :query_length] + test_step_size * grads[i][:, :query_length]    # should always use the initial grads
+            else:
+                temp_grads[i] = test_step_size * grads[i][:, :query_length]
         
+        token_pos = "start"     # control on input tokens by default
+        layer_ids = list(range(0, 32, 1))   # control on all layers by default
+        wrapped_model = control_on_layers(
+            layer_ids=layer_ids,
+            wrapped_model=wrapped_model,
+            grads=temp_grads,
+            query_length=query_length,
+            token_pos=token_pos,
+        )
+        input_with_suffix = [input + suffix for input in wrapped_model.generate(orig_input, do_sample=do_sample, **control_args)]
+        wrapped_model.reset()
+        _, outputs, loss, probs, logits, norms = get_verbalized_grads_from_wrapped_model(
+            inputs=input_with_suffix,
+            wrapped_model=wrapped_model,
+            tokenizer=tokenizer,
+            loss_fct=loss_fct,
+            targets=target,
+            verbalizer=verbalizer
+        )
+        if verbose:
+            print(f"Input w/ suffix: {input_with_suffix}")
+            print(f"Loss: {loss}")
+
+        del outputs, probs, logits, norms
+        
+        # Check if the loss is better than what we have seen so far
+        if loss < best_loss:
+            best_loss = loss
+            best_step_size = test_step_size
+            
+            # Check if the loss is below the threshold
+            if loss <= loss_threshold:
+                # print(f"Better Step-size found: {best_step_size}, Loss: {loss}")
+                return best_step_size
+        else:
+            # print(f"Step-size found: {test_step_size}, Loss: {loss}")
+            pass
+    
         # If not, scale down the absolute value of the step-size and continue
         current_step_size *= scale_factor
-    
-    print(f"Best step-size found: {best_step_size}, Loss: {best_loss}")
+    if verbose:
+        print(f"Best step-size found: {best_step_size}, Loss: {best_loss}")
     return best_step_size
 
 
-def KL_divergence(p, q, epsilon=1e-12):
+def KL_divergence(p, q, epsilon=1e-9):
     """Compuates KL divergence between two probability distributions
 
     Args:
@@ -510,5 +543,5 @@ def label_smoothing(one_hot_labels, smoothing=0.5):
         np.ndarray: Smoothed labels.
     """
     num_classes = one_hot_labels.shape[1]
-    smooth_labels = (1.0 - smoothing) * one_hot_labels + (smoothing / num_classes)
+    smooth_labels = (1.0 - smoothing) * one_hot_labels + (smoothing / num_classes) * np.ones_like(one_hot_labels)
     return smooth_labels